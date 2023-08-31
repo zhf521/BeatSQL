7d3e2c61c63cc9f631fab26b5a722343440d9198
zhf521 <3407085928@qq.com>
2023-08-31 22:56:35 +0800

    问题面板基本功能实现

--- src/components/MDViewer.vue
+++ src/components/MDViewer.vue
@@ -1,6 +1,18 @@
 <template>
+  <Viewer :value="value" :plugins="plugins" />
 </template>
 <script setup>
+import { Viewer } from '@bytemd/vue-next';
+// 插件：支持 GFM（自动链接文字、删除线、表格、任务列表）
+import gfm from '@bytemd/plugin-gfm';
+// 插件：支持代码高亮
+import heighlight from '@bytemd/plugin-highlight';
+import 'highlight.js/styles/default.css';
+// 引入github-markdown-css主题
+import 'github-markdown-css/github-markdown.css';
+import 'bytemd/dist/index.css';
+
+const plugins = [gfm(), heighlight()];
+const props = defineProps(['value']);
+
 </script>
-<style>
-</style>
--- src/components/QuestionBoard.vue
+++ src/components/QuestionBoard.vue
@@ -2,22 +2,67 @@
   <div id="questionBoard">
     <a-card v-if="props.level" id="questionCard">
       <!-- md展示区 -->
-      <MDViewer></MDViewer>
+      <MDViewer :value="props.level.content"></MDViewer>
       <a-divider></a-divider>
       <div>
-        <a-button>上一关</a-button>
-        <a-button>下一关</a-button>
-        <a-button>恭喜过关</a-button>
+        <a-button v-if="levelNum > 0" style="float: left;" @click="toPrevLevel">上一关</a-button>
+        <a-button v-if="levelNum < mainLevels.length - 1" type="primary" style="float: right;"
+          @click="toNextLevel">下一关</a-button>
+        <a-button v-if="levelNum === mainLevels.length - 1" type="primary" style="float: right;"
+          @click="doWin">恭喜过关</a-button>
       </div>
     </a-card>
     <a-card v-else>关卡加载失败</a-card>
   </div>
 </template>
 <script setup>
+import { computed, watch } from 'vue';
 import MDViewer from './MDViewer.vue';
+import { getCurrentLevelNum, getNextLevel, getPrevLevel } from '../levels';
+import mainLevels from '../levels/mainLevels';
+import { useRouter } from 'vue-router';
 // 接收LearnPage传过来的数据
 const props = defineProps(['level']);
-
-
+// 关卡在关卡数组中的索引数
+const levelNum = computed(() => {
+  return getCurrentLevelNum(props.level)
+});
+// router实例
+const router = useRouter();
+// 进入上一关
+const toPrevLevel = () => {
+  const toLevel = getPrevLevel(props.level);
+  if (toLevel) {
+    router.push(`/learn/${toLevel.key}`);
+  };
+};
+// 进入下一关
+const toNextLevel = () => {
+  const toLevel = getNextLevel(props.level);
+  if (toLevel) {
+    router.push(`/learn/${toLevel.key}`);
+  };
+};
+// 通关
+const doWin = () => {
+  alert('恭喜通关，有收获的话，欢迎给本项目一个 star 哦~');
+  window.open('https://github.com/zhf521/BeatSQL');
+};
+// 切换关卡时，回到顶部
+watch([levelNum], () => {
+  scrollTo({
+    top: 0,
+  });
+  const questionCardDom = document.getElementById('questionCard');
+  if (questionCardDom) {
+    questionCardDom.scrollTop = 0;
+  };
+});
 </script>
-<style></style>
+<style scoped>
+#questionBoard #questionCard {
+  max-height: calc(100vh - 100px);
+  min-height: 600px;
+  overflow-y: auto;
+}
+</style>
