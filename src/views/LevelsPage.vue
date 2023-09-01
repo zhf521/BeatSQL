1e72b08b950019b02e2c19d2a6071733492075fd
zhf521 <3407085928@qq.com>
2023-09-01 10:05:43 +0800

    关卡页面实现

--- src/views/LevelsPage.vue
+++ src/views/LevelsPage.vue
@@ -1,6 +1,43 @@
 <template>
-  关卡页面
+  <a-row>
+    <a-col :md="12" :xs="24">
+      <a-card title="主线关卡">
+        <a-list item-layout="horizontal" :data-source="mainLevels">
+          <template #renderItem="{ item,index }">
+            <a-list-item>
+              <a-list-item-meta :title="`${index+1}.${item.title}`">
+              </a-list-item-meta>
+              <template #actions>
+                <router-link :to="`/learn/${item.key}`">
+                  <a-button>挑战</a-button>
+                </router-link>
+              </template>
+            </a-list-item>
+          </template>
+        </a-list>
+      </a-card>
+    </a-col>
+    <a-col :md="12" :xs="24">
+      <a-card title="自定义关卡">
+        <a-list item-layout="horizontal" :data-source="customLevels">
+            <template #renderItem="{ item, index }">
+              <a-list-item>
+                <a-list-item-meta :title="`${index + 1}.${item.title}`">
+                </a-list-item-meta>
+                <template #actions>
+                  <router-link :to="`/learn/${item.key}`">
+                    <a-button>挑战</a-button>
+                  </router-link>
+                </template>
+              </a-list-item>
+            </template>
+          </a-list>
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
 <script  setup>
+import mainLevels from '../levels/mainLevels';
+import customLevels from '../levels/customLevels';
 </script>
 <style></style>
